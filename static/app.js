1c1bfe4edc6b5be96bcc358665740930830a6cbe
mazminier <32083336+mazminier@users.noreply.github.com>
2018-06-30 04:22:32 -0400

    update

--- static/app.js
+++ static/app.js
@@ -25,14 +25,14 @@ function buildPlot() {
     var data = [trace1];
 
     var layout = {
-      title: "District of Columbia Crashes Caused by Impaired Drivers in 2016",
+
       xaxis: {
         type: "linear",
         title: "Time of Day",
         nticks: "24",
         showgrid: "True",
-        tickvals: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'],
-        ticktext: ['12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM','2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM']
+        tickvals: ['', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'],
+        ticktext: ['', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM','2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM']
 
       },
       yaxis: {
